a692dada534579a65b9504908983a0931bc3074e
coin7star <coin7star@gmail.com>
2026-05-25 15:48:23 +0700

    Update supabaseClient.js

--- app/utils/supabaseClient.js
+++ app/utils/supabaseClient.js
@@ -1,8 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabase() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
+  const supabaseUrl =
+    process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    "https://dummy.supabase.co";
+
+  const supabaseAnonKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+    "dummy-anon-key";
+
+  return createClient(supabaseUrl, supabaseAnonKey);
 }
